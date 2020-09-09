d7a44e642f6bc8036014c9c0cc4a5a33a5a4abb0
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-09-09 17:46:46 +0300

    Fix.

--- config/events-web.php
+++ config/events-web.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
 use Yiisoft\Csrf\CsrfToken;
 use Yiisoft\Yii\Web\Event\BeforeRequest;
 
 return [
     BeforeRequest::class => [
-        fn (BeforeRequest $event, ContainerInterface $container) => CsrfToken::initialize(),
+        fn () => CsrfToken::initialize(),
     ],
 ];
