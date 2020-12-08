301f7ad11d96a996dae0447b78fb6b8f37d97fa0
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-12-08 20:13:17 +0300

    Add CsrfTokenGeneratorInterface to config.

--- config/web.php
+++ config/web.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Csrf\TokenGenerator\CsrfTokenGeneratorInterface;
+use Yiisoft\Csrf\TokenGenerator\RandomCsrfTokenGenerator;
 use Yiisoft\Csrf\TokenStorage\CsrfTokenStorageInterface;
 use Yiisoft\Csrf\TokenStorage\SessionCsrfTokenStorage;
 
 return [
+    CsrfTokenGeneratorInterface::class => RandomCsrfTokenGenerator::class,
     CsrfTokenStorageInterface::class => SessionCsrfTokenStorage::class,
 ];
