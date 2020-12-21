1aa050ce7f28341a41a3b0a898ff88f84a2daa8c
Alexander Makarov <sam@rmcreative.ru>
2020-12-21 22:23:42 +0300

    Update phpdoc

--- src/TokenFetcher/CsrfTokenFetcherInterface.php
+++ src/TokenFetcher/CsrfTokenFetcherInterface.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Yiisoft\Csrf\TokenFetcher;
 
 /**
- * Token fetcher returns the token string valid for the next request.
+ * Token fetcher returns currently valid token as string.
  *
  * @internal Do not use directly. See {@see CsrfToken::getValue()}.
  */
 interface CsrfTokenFetcherInterface
 {
     /**
-     * @return string The token string valid for the next request.
+     * @return string The currently valid token as string.
      */
     public function getValue(): string;
 }
