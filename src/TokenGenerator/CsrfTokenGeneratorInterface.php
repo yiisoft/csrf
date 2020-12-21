d5f235f15c3b4ce0dd54c1fb0cd3d764459794ba
Alexander Makarov <sam@rmcreative.ru>
2020-12-21 17:01:56 +0300

    Add another phpdoc piece

--- src/TokenGenerator/CsrfTokenGeneratorInterface.php
+++ src/TokenGenerator/CsrfTokenGeneratorInterface.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Yiisoft\Csrf\TokenGenerator;
 
+/**
+ * Token generator generates a new CSRF token.
+ * @internal Do not use directly.
+ */
 interface CsrfTokenGeneratorInterface
 {
     public function generate(): string;
