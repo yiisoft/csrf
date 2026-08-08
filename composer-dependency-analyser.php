e03cf73a2f44836fe511d3991a229d5b0ac8293b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 17:37:29 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -11,4 +11,8 @@ return (new Configuration())
     ->addPathToScan(__DIR__ . '/config', false)
     ->addPathToScan(__DIR__ . '/src', false)
     ->addPathToScan(__DIR__ . '/tests', true)
-    ->ignoreErrorsOnPackageAndPath('yiisoft/definitions', __DIR__ . '/config/di-web.php', [ErrorType::SHADOW_DEPENDENCY]);
+    // Used in "config/di-web.php", but not listed in "composer.json" because it's a shadow dependency of "yiisoft/di".
+    ->ignoreErrorsOnPackageAndPath('yiisoft/definitions', __DIR__ . '/config/di-web.php', [ErrorType::SHADOW_DEPENDENCY])
+    // Virtual packages: they only mark that a PSR interface implementation must be provided, so no direct code usage is expected.
+    ->ignoreErrorsOnPackage('psr/http-factory-implementation', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-message-implementation', [ErrorType::UNUSED_DEPENDENCY]);
