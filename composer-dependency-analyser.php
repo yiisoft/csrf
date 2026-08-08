bd835fbf8d68d6d7b143aadff5f2983613b6fcd1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 17:35:10 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,10 +3,12 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
-    ->addPathToScan(__DIR__ . '/config', isDev: false)
-    ->addPathToScan(__DIR__ . '/src', isDev: false)
-    ->addPathToScan(__DIR__ . '/tests', isDev: true);
+    ->addPathToScan(__DIR__ . '/config', false)
+    ->addPathToScan(__DIR__ . '/src', false)
+    ->addPathToScan(__DIR__ . '/tests', true)
+    ->ignoreErrorsOnPackageAndPath('yiisoft/definitions', __DIR__ . '/config/di-web.php', [ErrorType::SHADOW_DEPENDENCY]);
